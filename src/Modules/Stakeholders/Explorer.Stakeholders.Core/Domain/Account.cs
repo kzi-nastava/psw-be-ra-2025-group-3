30765e10830c8d4be774615c2f1e315bbb33d805
Njeno Ime <njen@email.com>
2025-11-15 21:05:40 +0100

    dodat Account.cs

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public enum AccountRole
+    {
+        Admin,
+        Author,
+        Tourist
+    }
+
+    public enum AccountStatus
+    {
+        Active,
+        Blocked
+    }
+    public class Account
+    {
+        public int Id { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+        public AccountRole Role { get; private set; }
+        public AccountStatus Status { get; private set; }
+
+        protected Account() { } 
+
+        public Account(string username, string password, string email, AccountRole role)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Role = role;
+            Status = AccountStatus.Active;
+        }
+
+        public void Block()
+        {
+            Status = AccountStatus.Blocked;
+        }
+
+        public void Unblock()
+        {
+            Status = AccountStatus.Active;
+        }
+    }
+}

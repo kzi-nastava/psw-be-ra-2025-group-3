31c3a148dd4d8dc72d7a8372e1cdc41c6aa4f77e
Marko Velimirovic <velimirovitsh@gmail.com>
2025-11-15 16:44:12 +0100

    Feat: Task 8 Pushing Again

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Explorer.Tours.API.Dtos;
+
+public class TourDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Difficulty { get; set; } // 0 = Easy, 1 = Medium, 2 = Hard
+    public int Status { get; set; } // 0 = Draft, 1 = Published
+    public decimal Price { get; set; }
+    public long AuthorId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public List<string> Tags { get; set; }
+}
--- src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
@@ -1,16 +1,32 @@
 ﻿using Explorer.Tours.Core.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Text.Json;
 
 namespace Explorer.Tours.Infrastructure.Database;
 
 public class ToursContext : DbContext
 {
     public DbSet<Equipment> Equipment { get; set; }
-
+    public DbSet<Tour> Tours { get; set; }
     public ToursContext(DbContextOptions<ToursContext> options) : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("tours");
+        modelBuilder.Entity<Tour>().HasIndex(t => t.AuthorId);
+
+        modelBuilder.Entity<Tour>()
+            .Property(t => t.Tags)
+            .HasColumnType("jsonb")
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                v => JsonSerializer.Deserialize<List<string>>(v, (JsonSerializerOptions)null) ?? new List<string>()
+            )
+            .Metadata.SetValueComparer(new ValueComparer<List<string>>(
+                (c1, c2) => c1.SequenceEqual(c2),
+                c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                c => c.ToList()
+            ));
     }
 }

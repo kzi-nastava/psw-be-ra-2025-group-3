6ed600f8a0e9e475581e0f128f152d068821fa74
Petar Bratić <bratic.petar03@gmail.com>
2025-11-17 20:37:26 +0100

    WIP: Save current progress

---
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/b-monuments.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO tours."Monuments"(
+    "Id", "Name", "Description", "Year", "Status", "Latitude", "Longitude")
+VALUES 
+(-1, 'Petrovaradinska tvrđava', 'Jedan od najvećih očuvanih tvrđavskih kompleksa u Evropi.', 1780, 'Active', 45.2520, 19.8618);
+
+INSERT INTO tours."Monuments"(
+    "Id", "Name", "Description", "Year", "Status", "Latitude", "Longitude")
+VALUES
+(-2, 'Spomenik Svetozaru Miletiću', 'Poznati spomenik u centru Novog Sada, rad Ivana Meštrovića.', 1939, 'Inactive', 45.2559, 19.8453);
+
+INSERT INTO tours."Monuments"(
+    "Id", "Name", "Description", "Year", "Status", "Latitude", "Longitude")
+VALUES
+(-3, 'Kuća Milutina Milankovića', 'Rodna kuća svetski poznatog naučnika Milutina Milankovića.', 1879, 'Active', 45.2441, 19.8517);

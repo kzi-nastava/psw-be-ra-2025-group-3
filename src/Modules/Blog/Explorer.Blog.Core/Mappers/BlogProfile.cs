a32d5ff022bd1a37565f8e3a80d992965191ad6a
c4ps63 <pavlekapuran1@gmail.com>
2025-11-19 22:58:52 +0100

    feat: add blog service with DTOs and repo interface

--- src/Modules/Blog/Explorer.Blog.Core/Mappers/BlogProfile.cs
+++ src/Modules/Blog/Explorer.Blog.Core/Mappers/BlogProfile.cs
@@ -1,11 +1,16 @@
 ﻿using AutoMapper;
+using Explorer.Blog.API.Dtos;
+using BlogEntity = Explorer.Blog.Core.Domain.Blogs.Blog;
+using BlogImageEntity = Explorer.Blog.Core.Domain.Blogs.BlogImage;
 
-namespace Explorer.Blog.Core.Mappers;
-
-public class BlogProfile : Profile
+namespace Explorer.Blog.Core.Mappers
 {
-    public BlogProfile()
+    public class BlogProfile : Profile
     {
-
+        public BlogProfile()
+        {
+            CreateMap<BlogDto, BlogEntity>().ReverseMap();
+            CreateMap<BlogImageDto, BlogImageEntity>().ReverseMap();
+        }
     }
 }
---
+++ src/Modules/Blog/Explorer.Blog.Core/UseCases/BlogService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Explorer.Blog.Core.Domain.RepositoryInterfaces;
+using BlogEntity = Explorer.Blog.Core.Domain.Blogs.Blog;
+
+namespace Explorer.Blog.Core.UseCases
+{
+    public class BlogService : IBlogService
+    {
+        private readonly IBlogRepository _repository;
+        private readonly IMapper _mapper;
+
+        public BlogService(IBlogRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public BlogDto CreateBlog(BlogDto blogDto)
+        {
+            var blog = _mapper.Map<BlogEntity>(blogDto);
+            var createdBlog = _repository.Add(blog);
+            return _mapper.Map<BlogDto>(createdBlog);
+        }
+
+        public BlogDto UpdateBlog(BlogDto blogDto)
+        {
+            var blog = _mapper.Map<BlogEntity>(blogDto);
+            var updatedBlog = _repository.Modify(blog);
+            return _mapper.Map<BlogDto>(updatedBlog);
+        }
+
+        public List<BlogDto> GetUserBlogs(int userId)
+        {
+            var blogs = _repository.GetByAuthor(userId);
+            return _mapper.Map<List<BlogDto>>(blogs);
+        }
+    }
+}

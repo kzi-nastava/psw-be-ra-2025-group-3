158446d5efdc38c95bbd42b224a60aa60c28f466
Njeno Ime <njen@email.com>
2025-11-23 19:11:49 +0100

    testovi prolaze

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/PersonDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/PersonDto.cs
@@ -8,9 +8,9 @@ public class PersonDto
     public bool IsActive { get; set; }
     public string Email { get; set; }
 
-    public string Username { get; set; }
+    public string? Username { get; set; }
 
-    public string Role {  get; set; }
+    public string? Role {  get; set; }
     public string? ProfilePictureUrl { get; set; }
     public string? Biography { get; set; }
     public string? Quote { get; set; }

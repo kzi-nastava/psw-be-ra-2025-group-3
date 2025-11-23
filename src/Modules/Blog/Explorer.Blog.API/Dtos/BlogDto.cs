919e3164cca7ef89e11e9dd2af9fc84228e93b00
c4ps63 <pavlekapuran1@gmail.com>
2025-11-23 19:41:46 +0100

    BlogDTO fixed

--- src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Explorer.Blog.API.Dtos
 {
     public class BlogDto
     {
         public long Id { get; set; }
+
+        [Required(ErrorMessage = "Naslov je obavezan")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Naslov mora biti između 3 i 200 karaktera")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Opis je obavezan")]
+        [StringLength(10000, MinimumLength = 10, ErrorMessage = "Opis mora biti između 10 i 10000 karaktera")]
         public string Description { get; set; }
+
         public DateTime CreationDate { get; set; }
+
         public int AuthorId { get; set; }
-        public List<BlogImageDto> Images { get; set; }
+
+        public List<BlogImageDto> Images { get; set; } = new List<BlogImageDto>();
     }
 }

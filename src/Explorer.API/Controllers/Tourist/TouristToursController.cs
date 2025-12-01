84073ab70569adcea6c3ee116065fdc1bc89db70
andjelakumazec <andjelakumazec5@gmail.com>
2025-12-01 16:53:23 +0100

    Dodavanje prikaza published tura turistima

---
+++ src/Explorer.API/Controllers/Tourist/TouristToursController.cs
@@ -0,0 +1,27 @@
+﻿using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Authoring;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/tours")]
+    [ApiController]
+    public class TouristToursController : ControllerBase
+    {
+        private readonly ITourService _tourService;
+
+        public TouristToursController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<TourDto>> GetPublishedTours()
+        {
+            var tours = _tourService.GetPublished();
+            return Ok(tours);
+        }
+    }
+}

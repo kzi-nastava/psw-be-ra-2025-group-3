bc02ff6f7608402918b6c15738d445e7d32422d2
Njeno Ime <njen@email.com>
2025-12-14 21:00:54 +0100

    implementacija signalR infrastrukture za podrsku web socketa i real-time notifikacija

---
+++ src/Explorer.API/Hubs/NotificationHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace Explorer.API.Hubs;
+
+[Authorize]
+public class NotificationHub : Hub
+{
+	public override async Task OnConnectedAsync()
+	{
+		var userId = Context.User?
+			.Claims
+			.FirstOrDefault(c =>
+				c.Type == "personId" ||
+				c.Type == ClaimTypes.NameIdentifier
+			)?.Value;
+
+		if (!string.IsNullOrEmpty(userId))
+		{
+			await Groups.AddToGroupAsync(
+				Context.ConnectionId,
+				userId
+			);
+		}
+
+		await base.OnConnectedAsync();
+	}
+}

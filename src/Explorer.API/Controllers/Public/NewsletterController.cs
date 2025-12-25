0263f765c781342326211bd9dcc97f9399aa1d47
unknown <emilija1510.s@gmail.com>
2025-12-25 15:10:24 +0100

    newsletter added

---
+++ src/Explorer.API/Controllers/Public/NewsletterController.cs
@@ -0,0 +1,27 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.Blog.Core.UseCases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Public
+{
+    [ApiController]
+    [Route("api/newsletter")]
+    public class NewsletterController : ControllerBase
+    {
+        private readonly NewsletterService _service;
+
+        public NewsletterController(NewsletterService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("subscribe")]
+        [AllowAnonymous]
+        public IActionResult Subscribe([FromBody] NewsletterSubscriptionDto dto)
+        {
+            _service.Subscribe(dto.Email);
+            return Ok();
+        }
+    }
+}
---
+++ src/Modules/Blog/Explorer.Blog.Core/UseCases/NewsletterService.cs
@@ -0,0 +1,32 @@
+﻿using Explorer.Blog.Core.Domain.Newsletter;
+using Explorer.Blog.Core.Domain.RepositoryInterfaces;
+
+namespace Explorer.Blog.Core.UseCases
+{
+    public class NewsletterService
+    {
+        private readonly INewsletterRepository _repository;
+
+        public NewsletterService(INewsletterRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Subscribe(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email is required.");
+
+            if (_repository.Exists(email))
+                throw new InvalidOperationException("Email already subscribed.");
+
+            var subscriber = new NewsletterSubscriber
+            {
+                Email = email,
+                SubscribedAt = DateTime.UtcNow
+            };
+
+            _repository.Add(subscriber);
+        }
+    }
+}
